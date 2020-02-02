cbb3f88f5bee85e43e126c74818d9ec77f41f42a
nihat <nihat@192.168.86.44>
2020-02-02 16:49:35 -0500

    First Commit and Push Feb 2, 2020

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,60 @@
+	package com.DataTypePackage;
+	
+	public class DataType {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+	/* Not 
+	 * A 
+	 * command or 
+	 * syntax*/
+	
+			/*This
+			is
+			a
+			test*/
+		
+			int x = 1;
+			int y= 2;
+			int j = 3;
+			
+			
+			// double data type
+					
+			double a = 1.5;
+			double d = 2.5;
+			
+			// character data type 
+			
+			char z = '1';
+			
+			// string is a non primitive data type, actually a class
+			
+			String name = "Shomir";
+			
+			// boolean data type is a primitive data type
+			
+			boolean b = true;
+			boolean c = false;
+			
+			// System.out.println("Hello World");
+			// System.out.println (a*d);
+			// System.out.println (b);
+			
+			System.out.println ("My name is" +" " + name);
+			
+			
+			
+			
+					
+		}
+	
+		public static void Addition() {
+			
+			
+		}
+		
+		
+		
+	}
